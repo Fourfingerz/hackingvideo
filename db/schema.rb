794f6a3ba2011605df717dc13f526ef2d979e4a0
SzeChan <chansd5@gmail.com>
2015-01-18 11:22:38 -0500

    Added Video Model For URLS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118061104) do
+ActiveRecord::Schema.define(version: 20150118160622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 20150118061104) do
 
   create_table "issues", force: true do |t|
     t.string   "topic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "videos", force: true do |t|
+    t.string   "name"
+    t.text     "video_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
