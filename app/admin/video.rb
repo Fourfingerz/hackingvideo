5784636b38e3a4368c533a69d48804d4f6b97089
SzeChan <chansd5@gmail.com>
2015-01-18 11:45:37 -0500

    Fixed Video As ActiveAdmin Resource

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -1,7 +1,6 @@
 ActiveAdmin.register Video do
   permit_params :name, :video_url
 
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
